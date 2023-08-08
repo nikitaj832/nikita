0236f8952e6f2069df6b279375fbc6623ea1bc4d
nikitaj832 <140581278+nikitaj832@users.noreply.github.com>
2023-08-08 20:51:26 +0530

    Add files via upload

---
+++ python/8.py
@@ -0,0 +1,42 @@
+#print("hello world")
+
+
+# for i in range(4,101,3):
+#    print(i)
+
+
+# for i in range(10):
+#    print(i)
+
+
+# num=int(input("enter any number"))
+# temp=num
+# rev=0
+# while(num>0):
+#      dig=num%10
+#      rev*10+dig
+
+
+
+# g="mohit"
+# print(g[2])
+
+
+# i="mohit"
+# print(len(i))
+
+
+# a="mohit"
+# for i in range(0,len(a),1):
+#     print(a[i])
+
+
+
+# a="mohit"
+# for i in range(0,len(a),2):
+#     print(a[i]) 
+
+
+    
+
+
---
+++ python/whatsapp.py
@@ -0,0 +1,2 @@
+import pywhatkit as kit 
+kit.sendwhatmsg("+916367621834","hello i am nikita jain",20,50)
